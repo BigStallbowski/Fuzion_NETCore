5165b56a4ce55a90f9a8720b2a9d726d0fd8154f
James Stallings <bigstallbowski@gmail.com>
2018-10-30 06:01:31 -0700

    Completed Db seeder with basic information

--- Fuzion.Core/Context/FuzionDbContext.cs
+++ Fuzion.Core/Context/FuzionDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Fuzion.Core.ModelConfigurations;
 using Fuzion.Core.Models;
--- Fuzion.Core/Context/FuzionDbSeeder.cs
+++ Fuzion.Core/Context/FuzionDbSeeder.cs
@@ -61,6 +61,48 @@ namespace Fuzion.Core.Context
                 _Logger.LogError($"Error in {nameof(FuzionDbContext)}: " + e.Message);
                 throw;
             }
+
+            var models = GetModels();
+            db.Models.AddRange(models);
+
+            try
+            {
+                var numAffected = await db.SaveChangesAsync();
+                _Logger.LogInformation($"Saved {numAffected} models");
+            }
+            catch (Exception e)
+            {
+                _Logger.LogError($"Error in {nameof(FuzionDbContext)}: " + e.Message);
+                throw;
+            }
+
+            var operatingSystems = GetOperatingSystems();
+            db.OS.AddRange(operatingSystems);
+
+            try
+            {
+                var numAffected = await db.SaveChangesAsync();
+                _Logger.LogInformation($"Saved {numAffected} operating systems");
+            }
+            catch (Exception e)
+            {
+                _Logger.LogError($"Error in {nameof(FuzionDbContext)}: " + e.Message);
+                throw;
+            }
+
+            var purposes = GetPurposes();
+            db.Purposes.AddRange(purposes);
+
+            try
+            {
+                var numAffected = await db.SaveChangesAsync();
+                _Logger.LogInformation($"Saved {numAffected} purposes");
+            }
+            catch (Exception e)
+            {
+                _Logger.LogError($"Error in {nameof(FuzionDbContext)}: " + e.Message);
+                throw;
+            }
         }
 
         private List<HardwareType> GetHardwareTypes()
@@ -71,7 +113,7 @@ namespace Fuzion.Core.Context
                 new HardwareType { Name = "Workstation" },
                 new HardwareType { Name = "Tablet" },
                 new HardwareType { Name = "Server" },
-                new HardwareType { Name = "Scanner"},
+                new HardwareType { Name = "Scanner" },
                 new HardwareType { Name = "Printer" }
             };
 
@@ -82,12 +124,52 @@ namespace Fuzion.Core.Context
         {
             var manufacturers = new List<Manufacturer>
             {
-                new Manufacturer {Name = "Dell"},
-                new Manufacturer {Name = "Fujitsu"},
-                new Manufacturer {Name = "Canon"}
+                new Manufacturer { Name = "Dell" },
+                new Manufacturer { Name = "Fujitsu" },
+                new Manufacturer { Name = "Canon" },
+                new Manufacturer { Name = "Samsung" },
+                new Manufacturer { Name = "Apple" }
             };
 
             return manufacturers;
+        }
+
+        private List<Model> GetModels()
+        {
+            var models = new List<Model>
+            {
+                new Model {Name = "Latitude 7530"},
+                new Model {Name = "FI-7160"},
+                new Model {Name = "Galaxy 9"},
+                new Model {Name = "iPhone X"},
+                new Model {Name = "Precision 3620"}
+            };
+
+            return models;
+        }
+
+        private List<OS> GetOperatingSystems()
+        {
+            var operatingSystems = new List<OS>
+            {
+                new OS {Name = "Windows 7"},
+                new OS {Name = "Windows 10"},
+                new OS {Name = "Android"},
+                new OS {Name = "iOS"}
+            };
+
+            return operatingSystems;
+        }
+
+        private List<Purpose> GetPurposes()
+        {
+            var purposes = new List<Purpose>
+            {
+                new Purpose {Name = "Production"},
+                new Purpose {Name = "Development"}
+            };
+
+            return purposes;
         }
     }
 }
