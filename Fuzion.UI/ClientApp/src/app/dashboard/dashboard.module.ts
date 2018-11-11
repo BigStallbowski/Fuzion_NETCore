ab872070e5d03d18fd9c8cd7f6d069161789b577
James Stallings <bigstallbowski@gmail.com>
2018-11-11 15:00:20 -0700

    Hardcoded 1st draft of dashboard completed

--- Fuzion.UI/ClientApp/src/app/dashboard/dashboard.module.ts
+++ Fuzion.UI/ClientApp/src/app/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { ChartistModule } from 'ng-chartist';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatchHeightModule } from '../shared/directives/match-height.directive';
 import { DashboardComponent } from './dashboard.component';
@@ -10,6 +11,7 @@ import { DashboardComponent } from './dashboard.component';
     imports: [
         CommonModule,
         DashboardRoutingModule,
+        ChartistModule,
         NgbModule,
         MatchHeightModule
     ],
